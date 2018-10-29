aa66ba0149c96b01f0f548a316180acf312bda34
lanty <2500709366@qq.com>
2018-10-29 08:34:43 +0800

    上传测试（没用）

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-//
-// Vue.use(Router)
-//
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       redirect: '/msite'
-//     }
-//   ]
-// })
+import Vue from 'vue'
+import Router from 'vue-router'
+import Created from '../pages/Created'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
