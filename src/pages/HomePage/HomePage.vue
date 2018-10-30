b676b2a5a8c191ad05496a54a3e3f135b5ca6cca
lanty <2500709366@qq.com>
2018-10-30 14:35:20 +0800

    首页

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,54 +1,446 @@
 <template>
   <div>
+    <!--轮播-->
     <nav class="msite_nav border-1px">
       <div class="swiper-container">
         <div class="swiper-wrapper">
           <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/text.jpg">
-              </div>
-              <span>甜品饮品</span>
-            </a>
+            <img src="./images/broadcast/broadcast1.jpg">
           </div>
           <div class="swiper-slide">
-            <a href="javascript:" class="link_to_food">
-              <div class="food_container">
-                <img src="./images/text.jpg">
-              </div>
-              <span>甜品饮品</span>
-            </a>
+            <img src="./images/broadcast/broadcast2.jpg">
+          </div>
+          <div class="swiper-slide">
+            <img src="./images/broadcast/broadcast3.png">
+          </div>
+          <div class="swiper-slide">
+            <img src="./images/broadcast/broadcast4.jpg">
+          </div>
+          <div class="swiper-slide">
+            <img src="./images/broadcast/broadcast5.png">
           </div>
         </div>
         <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
       </div>
     </nav>
+    <!--居中内容-->
+    <div class="content">
+      <!--美食-->
+      <div id="foodRecommend">
+        <!--小标题-->
+        <p class="smallTitle">
+          <a href="javascript:;" title="推荐食谱">
+            <span>推荐食谱</span>
+            <br>
+            <span>RECOMMEND</span>
+          </a>
+        </p>
+        <!--美食推荐-->
+        <ul class="recommend">
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend1.png" alt="">
+            </a>
+            <a>椰汁桂花糕</a>
+            <p>by &nbsp;<a href="javascript:;">lanty</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend1.png" alt="">
+            </a>
+            <a>椰汁桂花糕</a>
+            <p>by &nbsp;<a href="javascript:;">lanty</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend1.png" alt="">
+            </a>
+            <a>椰汁桂花糕</a>
+            <p>by &nbsp;<a href="javascript:;">lanty</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend1.png" alt="">
+            </a>
+            <a>椰汁桂花糕</a>
+            <p>by &nbsp;<a href="javascript:;">lanty</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend2.png" alt="">
+            </a>
+            <a>芹菜炒肉</a>
+            <p>by &nbsp;<a href="javascript:;">林吵吵</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend2.png" alt="">
+            </a>
+            <a>芹菜炒肉</a>
+            <p>by &nbsp;<a href="javascript:;">林吵吵</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend2.png" alt="">
+            </a>
+            <a>芹菜炒肉</a>
+            <p>by &nbsp;<a href="javascript:;">林吵吵</a></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/recommend/recommend2.png" alt="">
+            </a>
+            <a>芹菜炒肉</a>
+            <p>by &nbsp;<a href="javascript:;">林吵吵</a></p>
+          </li>
+        </ul>
+      </div>
+      <!--商城-->
+      <div id="shop">
+        <!--小标题-->
+        <p class="smallTitle">
+          <a href="javascript:;" title="商城精选">
+            <span>商城精选</span>
+            <br>
+            <span>ELABORATE WORKS</span>
+          </a>
+        </p>
+        <!--商城精选-->
+        <ul class="recommend">
+          <li>
+            <a href="javascript:;">
+              <img src="./images/shopimg/shopimg1.png" alt="">
+            </a>
+            <a>九阳320ml小红绳保温杯316L...</a>
+            <p class="price">￥99.00</p>
+            <p>月销200件 <span>包邮</span></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/shopimg/shopimg1.png" alt="">
+            </a>
+            <a>九阳320ml小红绳保温杯316L...</a>
+            <p class="price">￥99.00</p>
+            <p>月销200件 <span>包邮</span></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/shopimg/shopimg1.png" alt="">
+            </a>
+            <a>九阳320ml小红绳保温杯316L...</a>
+            <p class="price">￥99.00</p>
+            <p>月销200件 <span>包邮</span></p>
+          </li>
+          <li>
+            <a href="javascript:;">
+              <img src="./images/shopimg/shopimg1.png" alt="">
+            </a>
+            <a>九阳320ml小红绳保温杯316L...</a>
+            <p class="price">￥99.00</p>
+            <p>月销200件 <span>包邮</span></p>
+          </li>
+        </ul>
+      </div>
+      <!--新闻+活动-->
+      <div id="newAct">
+        <div id="left">
+          <!--标题栏-->
+          <p>
+            <span>随笔</span>
+            <span>JOTTINGS</span>
+          </p>
+          <!--内容区-->
+          <ul>
+            <li>
+              <p>网红小姐姐和健身达人推崇的0脂肪来了！</p>
+              <div class="newsImg">
+                <img src="./images/news/news1.png" alt="newsImg">
+              </div>
+              <p>
+                随着人们健康意识的增强，高热量高脂肪的食物正在慢慢离我们远去，0脂肪时代已正式开启！！！
+                7月31日午后，在三里屯人气沙拉店齐聚众多网红和健身爱好者，举办了一年一度的【丘比沙拉汁PARTY】。在这样一个“1日限定”的丘比空间中，除了人见人爱的丘比娃娃和丘比商品以外，更是可以在品尝到最新流行美食的同时，聊着与健康相关的话题，现场欢声不断。
+                【丘比沙拉汁PARTY】被打造成小清新风格，可爱的丘比娃娃陪伴着大家， 在场的每个人都沉浸其中……
+              </p>
+              <p>
+                <i class="iconfont icon-author"></i>
+                <a href="">少油少盐</a>
+                发表日期 2017-09-10
+                <span>
+                  <i class="iconfont icon-eyes"></i>
+                  7
+                  <i class="iconfont icon-share"></i>
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>网红小姐姐和健身达人推崇的0脂肪来了！</p>
+              <div class="newsImg">
+                <img src="./images/news/news2.png" alt="newsImg">
+              </div>
+              <p>
+                随着人们健康意识的增强，高热量高脂肪的食物正在慢慢离我们远去，0脂肪时代已正式开启！！！
+                7月31日午后，在三里屯人气沙拉店齐聚众多网红和健身爱好者，举办了一年一度的【丘比沙拉汁PARTY】。在这样一个“1日限定”的丘比空间中，除了人见人爱的丘比娃娃和丘比商品以外，更是可以在品尝到最新流行美食的同时，聊着与健康相关的话题，现场欢声不断。
+                【丘比沙拉汁PARTY】被打造成小清新风格，可爱的丘比娃娃陪伴着大家， 在场的每个人都沉浸其中……
+              </p>
+              <p>
+                <i class="iconfont icon-author"></i>
+                <a href="">少油少盐</a>
+                发表日期 2017-09-10
+                <span>
+                  <i class="iconfont icon-eyes"></i>
+                  7
+                  <i class="iconfont icon-share"></i>
+                </span>
+              </p>
+            </li>
+            <li>
+              <p>网红小姐姐和健身达人推崇的0脂肪来了！</p>
+              <div class="newsImg">
+                <img src="./images/news/news3.png" alt="newsImg">
+              </div>
+              <p>
+                随着人们健康意识的增强，高热量高脂肪的食物正在慢慢离我们远去，0脂肪时代已正式开启！！！
+                7月31日午后，在三里屯人气沙拉店齐聚众多网红和健身爱好者，举办了一年一度的【丘比沙拉汁PARTY】。在这样一个“1日限定”的丘比空间中，除了人见人爱的丘比娃娃和丘比商品以外，更是可以在品尝到最新流行美食的同时，聊着与健康相关的话题，现场欢声不断。
+                【丘比沙拉汁PARTY】被打造成小清新风格，可爱的丘比娃娃陪伴着大家， 在场的每个人都沉浸其中……
+              </p>
+              <p>
+                <i class="iconfont icon-author"></i>
+                <a href="">少油少盐</a>
+                发表日期 2017-09-10
+                <span>
+                  <i class="iconfont icon-eyes"></i>
+                  7
+                  <i class="iconfont icon-share"></i>
+                </span>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div id="right"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Swiper from 'swiper'
 import 'swiper/dist/css/swiper.min.css'
+
 export default {
   mounted () {
     /* eslint-disable no-new */
     new Swiper('.swiper-container', {
-      // pagination: {
-      //   el: '.swiper-pagination'
-      // },
-      // autoplay: 3000
-      // // autoplayStopOnLast: true,
-      // // loop: true
-
-
+      loop: true,
       autoplay: {
-        stopOnLastSlide: false,
-        delay: 1000
+        stopOnLastSlide: false
       }
     })
   }
 }
 </script>
+
 <style>
+  /*
+  轮播
+  */
+  .swiper-container {
+    width: 100%;
+    height: 400px;
+  }
+  swiper-wrapper,.swiper-slide>img {
+    height: 100%;
+    width: 100%;
+  }
+  /*居中内容*/
+  .content {
+    width: 1000px;
+    margin: 20px auto;
+    /*border: 1px solid black;*/
+    height: 5000px;
+  }
+  /*
+  推荐
+  */
+  #foodRecommend {
+    width: 100%;
+    height: 700px;
+    line-height: 1.5;
+  }
+  /*小标题*/
+  .smallTitle {
+    text-align: center;
+    width: 500px;
+    height: 80px;
+    margin: 0px auto;
+    padding-top: 15px;
+    border-bottom: 1px solid #5A3721;
+    position: relative;
+  }
+  .smallTitle:after {
+    content: '';
+    top: 100%;
+    left: 48%;
+    position: absolute;
+    width: 0;
+    border:10px solid transparent;
+    border-top-color: #5A3721;
+  }
+  .smallTitle>a{
+    text-decoration: none;
+    outline: 0;
+    transition: all .3s ease-in-out 0s;
+    line-height: 1.2;
+    color: #5A3721;
+    font-size: 26px;
+  }
+  .smallTitle>a>span:nth-of-type(1) {
+    width: 100px;
+    font-size: 22px;
+    margin: 0 auto;
+  }
+  /*推荐内容*/
+  .recommend {
+    width: 100%;
+    margin-top: 25px;
+    display: flex;
+    justify-content: space-between;
+    vertical-align: center;
+    flex-wrap: wrap;
+  }
+  .recommend>li {
+    width: 230px;
+    height: 300px;
+    float: left;
+    overflow: hidden;
+  }
+  .recommend>li>a:first-of-type {
+    width: 100%;
+    height: 220px;
+    display: inline-block;
+    overflow: hidden;
+  }
+  /*图片放大*/
+  .recommend>li>a>img {
+    width: 100%;
+    height: 220px;
+    transition: all 0.4s linear;
+  }
+  .recommend>li>a>img:hover {
+    transform: scale(1.1);
+  }
+  .recommend>li>p {
+    color: gray;
+    font-size: 14px;
+    clear: both;
+  }
+  .recommend>li a {
+    color: black;
+    transition: all 0.2s linear;
+  }
+  .recommend>li a:hover {
+    color: #F08E02;
+  }
+  /*
+  商城精选
+  */
+  #shop {
+    background-color: #2e6da4;
+    width: 100%;
+    height: 420px;
+    line-height: 1.5;
+  }
+  .price {
+    font-size: 18px!important;
+    color: red !important;
+  }
+  .recommend>li>p>span {
+    float: right;
+    padding-right: 2px;
+  }
+  /*
+  新闻和活动
+  */
+  #newAct {
+    width: 100%;
+    height: 1000px;
+    margin-top: 40px;
+  }
+  #left {
+    width: 680px;
+    height: 1000px;
+    border: 1px solid blueviolet;
+    display: inline-block;
+  }
+  /*标题栏*/
+  #left>p {
+    height: 100px;
+    border-bottom: 1px solid gray;
+    background: url("./images/news/newbg.gif") no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+    line-height: 1.3;
+    font-size: 20px;
+    margin: 0px 30px;
+    padding-bottom: 10px;
+  }
+  #left>p>span:first-child {
+    font-size: 24px;
+    margin-right: 20px;
+  }
+  /*内容区*/
+  #left>ul {
+    /*margin-top: 20px;*/
+    /*border: 1px solid black;*/
+    margin: 0px 30px;
+  }
+  #left>ul>li {
+    /*margin-top: 20px;*/
+    /*border: 1px solid red;*/
+    margin: 20px 0px;
+  }
+  #left>ul>li>p:first-child {
+    font-size: 20px;
+    line-height: 1.5;
+    font-weight: normal;
+    margin: 10px 0;
+  }
+  #left .newsImg {
+    width: 250px;
+    height: 140px;
+    text-align: center;
+    /*display: table-cell;*/
+    border-radius: 8px;
+    display: inline-block;
+    overflow: hidden;
+    vertical-align: middle;
+    /*border: 1px solid blueviolet;*/
+  }
+  #left .newsImg>img {
+    max-width: 100%;
+}
+  #left>ul>li>p:nth-of-type(2) {
+    width: 350px;
+    float: right;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 7;
+    -webkit-box-orient: vertical;
+    text-indent: 2em;
+    line-height: 1.4;
+    font-size: 15px;
+    color: #585a44;
+  }
+
+
+
+
+  #right {
+    width: 300px;
+    height: 500px;
+    float: right;
+    border: 1px solid royalblue;
+  }
 </style>
